8607eaf9046b7fd3b4ab215b6c308869d63eeafc
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-07-07 12:40:38 +0300

    Реализация класса Task для хранения информации о задаче

---
+++ src/Task.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+
+public class Task {
+    private final int id;
+    private String name;
+    private String description;
+    private TaskStatus status;
+
+    public Task(int id, String name, String description, TaskStatus status) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
