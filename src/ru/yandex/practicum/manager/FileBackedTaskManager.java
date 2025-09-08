bf6e5803d250720c1b1b7e901f3b5c3dafa38335
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-09-08 12:03:19 +0300

    Сделана начальная имлпементация класса менеджера задач с автосохранением

---
+++ src/ru/yandex/practicum/manager/FileBackedTaskManager.java
@@ -0,0 +1,87 @@
+package ru.yandex.practicum.manager;
+
+import ru.yandex.practicum.tasks.Epic;
+import ru.yandex.practicum.tasks.SubTask;
+import ru.yandex.practicum.tasks.Task;
+
+public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager{
+
+    @Override
+    public void deleteTasks() {
+        super.deleteTasks();
+        save();
+    }
+
+    @Override
+    public void deleteEpics() {
+        super.deleteEpics();
+        save();
+    }
+
+    @Override
+    public void deleteSubTasks() {
+        super.deleteSubTasks();
+        save();
+    }
+
+    @Override
+    public int createTask(Task task) {
+        int id = super.createTask(task);
+        save();
+        return id;
+    }
+
+    @Override
+    public int createEpic(Epic epic) {
+        int id = super.createEpic(epic);
+        save();
+        return id;
+    }
+
+    @Override
+    public int createSubTask(SubTask subTask) {
+        int id = super.createSubTask(subTask);
+        save();
+        return id;
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubTask(SubTask subTask) {
+        super.updateSubTask(subTask);
+        save();
+    }
+
+    @Override
+    public void deleteTaskById(int taskId) {
+        super.deleteTaskById(taskId);
+        save();
+    }
+
+    @Override
+    public void deleteEpicById(int epicId) {
+        super.deleteEpicById(epicId);
+        save();
+    }
+
+    @Override
+    public void deleteSubTaskById(int subTaskId) {
+        super.deleteSubTaskById(subTaskId);
+        save();
+    }
+
+    private void save() {
+
+    }
+}
