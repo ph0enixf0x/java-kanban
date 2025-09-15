a7accb20dc7aa10a644e248254be9d460704c57e
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-09-15 17:55:09 +0300

    Добавлены тесты на методы перевода задач в строку и обратно

--- test/ru/yandex/practicum/tasks/EpicTest.java
+++ test/ru/yandex/practicum/tasks/EpicTest.java
@@ -20,4 +20,23 @@ class EpicTest {
         assertEquals(0, epic1.getSubtasksIds().size(),
                 "Должно быть нельзя добавить эпик как подзадачу самому себе");
     }
+
+    @Test
+    void getEpicAsString() {
+        assertEquals("0,EPIC,Тестовый эпик,NEW,Описание тестового эпика,",
+                Epic.toString(new Epic("Тестовый эпик", "Описание тестового эпика")),
+                "Строка новой подзадачи не совпадает с ожидаемой");
+    }
+
+    @Test
+    void getModifiedEpicAsString() {
+        Epic task = new Epic("Тестовый эпик[] 1", "Описание тест&&^%ового эпика");
+        task.setId(4);
+        task.addSubTask(1);
+        task.addSubTask(5);
+        task.setStatus(TaskStatus.DONE);
+        assertEquals("4,EPIC,Тестовый эпик[] 1,DONE,Описание тест&&^%ового эпика,1,5",
+                Epic.toString(task),
+                "Строка измененной подзадачи не совпадает с ожидаемой");
+    }
 }
--- test/ru/yandex/practicum/tasks/TaskTest.java
+++ test/ru/yandex/practicum/tasks/TaskTest.java
@@ -13,4 +13,58 @@ class TaskTest {
         assertEquals(task2, task1, "Задачи с одинаковыми id должны быть идентичны");
     }
 
+    @Test
+    void getTaskAsString() {
+        assertEquals("0,TASK,Тестовая задача,NEW,Описание тестовой задачи",
+                Task.toString(new Task("Тестовая задача", "Описание тестовой задачи")),
+                "Строка новой задачи не совпадает с ожидаемой");
+    }
+
+    @Test
+    void getModifiedTaskAsString() {
+        Task task = new Task("Тестовая задача[] 1", "Описание тестов$#$ой задачи");
+        task.setId(4);
+        task.setStatus(TaskStatus.IN_PROGRESS);
+        assertEquals("4,TASK,Тестовая задача[] 1,IN_PROGRESS,Описание тестов$#$ой задачи",
+                Task.toString(task),
+                "Строка измененной задачи не совпадает с ожидаемой");
+    }
+
+    @Test
+    void getNewTaskFromString() {
+        assertEquals(new Task("Тестовая задача", "Описание тестовой задачи"),
+                Task.fromString("0,TASK,Тестовая задача,NEW,Описание тестовой задачи"),
+                "Объект задачи, созданный из строки, не совпадает с ожидаемым");
+    }
+
+    @Test
+    void getModifiedTaskIdFromString() {
+        assertEquals(4,
+                Task.fromString("4,TASK,Тестовая задача[] 1,IN_PROGRESS,Описание тестовой задачи").getId(),
+                "Идентификатор модифицированной задачи не соответствует ожидаемому");
+    }
+
+    @Test
+    void getModifiedTaskNameFromString() {
+        assertEquals("Тестовая задача[] 1",
+                Task.fromString("4,TASK,Тестовая задача[] 1,IN_PROGRESS,Описание тестовой задачи").getName(),
+                "Название модифицированной задачи не соответствует ожидаемому");
+    }
+
+    @Test
+    void getModifiedTaskStatusFromString() {
+        assertEquals(TaskStatus.IN_PROGRESS,
+                Task.fromString("4,TASK,Тестовая задача[] 1,IN_PROGRESS,Описание тестовой задачи")
+                        .getStatus(),
+                "Статус модифицированной задачи не соответствует ожидаемому");
+    }
+
+    @Test
+    void getModifiedTaskDescriptionFromString() {
+        assertEquals("Описание тестов$#$ой задачи",
+                Task.fromString("4,TASK,Тестовая задача[] 1,IN_PROGRESS,Описание тестов$#$ой задачи")
+                        .getDescription(),
+                "Описание модифицированной задачи не соответствует ожидаемому");
+    }
+
 }
