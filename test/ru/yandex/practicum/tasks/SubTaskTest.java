a5e935218586120a43ffcff5944cc888b5545fec
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-09-15 18:38:47 +0300

    Revert "Добавлены тесты на методы перевода задач в строку и обратно"
    This reverts commit a7accb20dc7aa10a644e248254be9d460704c57e.

--- test/ru/yandex/practicum/tasks/SubTaskTest.java
+++ test/ru/yandex/practicum/tasks/SubTaskTest.java
@@ -12,30 +12,4 @@ class SubTaskTest {
         SubTask subTask2 = new SubTask("Такая же подзадача", "У этой подзадачи такой же id", 1);
         assertEquals(subTask2, subTask1, "Подзадачи с одинаковым id должны быть идентичны");
     }
-
-    @Test
-    void getSubTaskAsString() {
-        assertEquals("0,SUBTASK,Тестовая подзадача,NEW,Описание тестовой подзадачи,0",
-                SubTask.toString(new SubTask("Тестовая подзадача", "Описание тестовой подзадачи", 0)),
-                "Строка новой подзадачи не совпадает с ожидаемой");
-    }
-
-    @Test
-    void getModifiedSubTaskAsString() {
-        SubTask task = new SubTask("Тестовая подзадача[] 1", "Описание тестов$#$ой подзадачи", 0);
-        task.setId(4);
-        task.setStatus(TaskStatus.IN_PROGRESS);
-        assertEquals("4,SUBTASK,Тестовая подзадача[] 1,IN_PROGRESS,Описание тестов$#$ой подзадачи,0",
-                SubTask.toString(task),
-                "Строка измененной подзадачи не совпадает с ожидаемой");
-    }
-
-    @Test
-    void getModifiedSubTaskEpicIdFromString() {
-        assertEquals(21,
-                SubTask.fromString("4,SUBTASK,Тестовая подзадача[] 1,IN_PROGRESS,Описание тестов$#$ой подзадачи,21")
-                        .getEpicId(),
-                "Идентификатор эпика модифицированной подзадачи не соответствует ожидаемому");
-    }
-
 }
