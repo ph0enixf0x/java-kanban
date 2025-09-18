c7800df32ba9df1c7d529dd0c680dfbec655f2f4
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-09-18 11:40:45 +0300

    Убраны тесты на проверку перевода строки в задачу

--- test/ru/yandex/practicum/manager/FileBackedTaskManagerTest.java
+++ test/ru/yandex/practicum/manager/FileBackedTaskManagerTest.java
@@ -25,56 +25,6 @@ class FileBackedTaskManagerTest {
     }
 
     @Test
-    void checkConvertedTaskId() {
-        assertEquals(4,
-                manager.fromString("4,TASK,Первая задача,Описание первой задачи,NEW,").getId(),
-                "Идентификатор сгенерированной задачи не соответствует ожидаемому");
-    }
-
-    @Test
-    void checkConvertedTaskType() {
-        assertEquals(TaskType.TASK,
-                manager.fromString("4,TASK,Первая задача,Описание первой задачи,NEW,").getType(),
-                "Тип сгенерированной задачи не соответствует ожидаемому");
-    }
-
-    @Test
-    void checkConvertedTaskName() {
-        assertEquals("Первая задача",
-                manager.fromString("4,TASK,Первая задача,Описание первой задачи,NEW,").getName(),
-                "Название сгенерированной задачи не соответствует ожидаемому");
-    }
-
-    @Test
-    void checkConvertedTaskDescription() {
-        assertEquals("Описание первой задачи",
-                manager.fromString("4,TASK,Первая задача,Описание первой задачи,NEW,").getDescription(),
-                "Описание сгенерированной задачи не соответствует ожидаемому");
-    }
-    @Test
-    void checkConvertedTaskStatus() {
-        assertEquals(TaskStatus.NEW,
-                manager.fromString("4,TASK,Первая задача,Описание первой задачи,NEW,").getStatus(),
-                "Статус сгенерированной задачи не соответствует ожидаемому");
-    }
-
-    @Test
-    void checkConvertedSubTaskEpicId() {
-        SubTask subTask = (SubTask) manager.fromString("4,SUBTASK,Подзадача один,Первая подзадача первого эпика,NEW,12");
-        assertEquals(12,
-                subTask.getEpicId(),
-                "Идентификатор эпика сгенерированной подзадачи не соответствует ожидаемому");
-    }
-
-    @Test
-    void checkConvertedEpicSubtaskIds() {
-        Epic epic = (Epic) manager.fromString("3,EPIC,Первый эпик,Описание первого эпика,NEW,4,5,6");
-        assertEquals("[4, 5, 6]",
-                epic.getSubtasksIds().toString(),
-                "Список идентификаторов подзадач сгенерированного эпика не соответствует ожидаемому");
-    }
-
-    @Test
     void checkLoadFromEmptyFile() {
         try {
             File testFile = File.createTempFile("test", ".txt");
