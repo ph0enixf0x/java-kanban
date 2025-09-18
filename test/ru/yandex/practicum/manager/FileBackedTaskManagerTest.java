26952bbe48b2ea600e2499dc49a1ef4fac9b4453
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-09-18 11:41:31 +0300

    Убран отлов ошибки IOException из тестов

--- test/ru/yandex/practicum/manager/FileBackedTaskManagerTest.java
+++ test/ru/yandex/practicum/manager/FileBackedTaskManagerTest.java
@@ -25,75 +25,58 @@ class FileBackedTaskManagerTest {
     }
 
     @Test
-    void checkLoadFromEmptyFile() {
-        try {
-            File testFile = File.createTempFile("test", ".txt");
-            assertNotNull(FileBackedTaskManager.loadFromFile(testFile),
+    void checkLoadFromEmptyFile() throws IOException {
+        File testFile = File.createTempFile("test", ".txt");
+        assertNotNull(FileBackedTaskManager.loadFromFile(testFile),
                     "Не удалось загрузить файловый менеджер из пустого файла");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
     }
 
     @Test
-    void checkLoadFromEmptyFileWithHeader() {
-        try {
+    void checkLoadFromEmptyFileWithHeader() throws IOException {
             File testFile = File.createTempFile("test", ".txt");
             try (FileWriter fr = new FileWriter(testFile)) {
-                String header = "id,type,name,description,status,info\n";
+                String header = "id,type,name,status,description,epic\n";
                 fr.write(header);
             }
             assertNotNull(FileBackedTaskManager.loadFromFile(testFile),
                     "Не удалось загрузить файловый менеджер из пустого файла");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
     }
 
     @Test
-    void checkLoadFromNormalFile() {
-        try {
-            File testFile = File.createTempFile("test", ".txt");
-            try (FileWriter fr = new FileWriter(testFile)) {
-                String header = """
+    void checkLoadFromNormalFile() throws IOException {
+        File testFile = File.createTempFile("test", ".txt");
+        try (FileWriter fr = new FileWriter(testFile)) {
+            String header = """
+                    id,type,name,description,status,info
+                    1,TASK,Первая задача,Описание первой задачи,NEW,
+                    4,SUBTASK,Подзадача один,Первая подзадача первого эпика,NEW,3
+                    3,EPIC,Первый эпик,Описание первого эпика,NEW,4
+                    """;
+            fr.write(header);
+        }
+        FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile(testFile);
+        assertEquals(3,
+                manager.getTasks().size() + manager.getSubTasks().size() + manager.getEpics().size(),
+                "Размер загруженных задач в менеджере не совпадает с ожидаемым");
+    }
+
+    @Test
+    void checkSaveFile() throws IOException {
+        File testFile = File.createTempFile("test", ".txt");
+        FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile(testFile);
+        manager.createTask(new Task("Первая задача", "Описание первой задачи"));
+        manager.createTask(new Task("Первая задача", "Описание первой задачи"));
+        manager.createTask(new Task("Первая задача", "Описание первой задачи"));
+        manager.createTask(new Task("Первая задача", "Описание первой задачи"));
+
+        assertEquals("""
                         id,type,name,description,status,info
                         1,TASK,Первая задача,Описание первой задачи,NEW,
-                        4,SUBTASK,Подзадача один,Первая подзадача первого эпика,NEW,3
-                        3,EPIC,Первый эпик,Описание первого эпика,NEW,4
-                        """;
-                fr.write(header);
-            }
-            FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile(testFile);
-            assertEquals(3,
-                    manager.getTasks().size() + manager.getSubTasks().size() + manager.getEpics().size(),
-                    "Размер загруженных задач в менеджере не совпадает с ожидаемым");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Test
-    void checkSaveFile() {
-        try {
-            File testFile = File.createTempFile("test", ".txt");
-            FileBackedTaskManager manager = FileBackedTaskManager.loadFromFile(testFile);
-            manager.createTask(new Task("Первая задача", "Описание первой задачи"));
-            manager.createTask(new Task("Первая задача", "Описание первой задачи"));
-            manager.createTask(new Task("Первая задача", "Описание первой задачи"));
-            manager.createTask(new Task("Первая задача", "Описание первой задачи"));
-
-            assertEquals("""
-                            id,type,name,description,status,info
-                            1,TASK,Первая задача,Описание первой задачи,NEW,
-                            2,TASK,Первая задача,Описание первой задачи,NEW,
-                            3,TASK,Первая задача,Описание первой задачи,NEW,
-                            4,TASK,Первая задача,Описание первой задачи,NEW,
-                            """,
-                    Files.readString(testFile.toPath()),
-                    "Сохраненный менеджером файл не соответствует ожидаемому");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+                        2,TASK,Первая задача,Описание первой задачи,NEW,
+                        3,TASK,Первая задача,Описание первой задачи,NEW,
+                        4,TASK,Первая задача,Описание первой задачи,NEW,
+                        """,
+                Files.readString(testFile.toPath()),
+                "Сохраненный менеджером файл не соответствует ожидаемому");
     }
 }
