56a6f5857996f0bc49b11730e2dfd84a4311863b
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-04 15:27:43 +0300

    Убраны лишние импорты

--- src/ru/yandex/practicum/manager/InMemoryTaskManager.java
+++ src/ru/yandex/practicum/manager/InMemoryTaskManager.java
@@ -9,7 +9,6 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class InMemoryTaskManager implements TaskManager {
     protected int taskCounter;
