d63d2c7a3d64cf859a021d63e05e95577f3eeb33
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-11 14:22:29 +0300

    Добавлены методы отправки ответов в базовый класс обработчик

--- src/ru/yandex/practicum/server/BaseHandler.java
+++ src/ru/yandex/practicum/server/BaseHandler.java
@@ -1,11 +1,26 @@
 package ru.yandex.practicum.server;
 
+import com.sun.net.httpserver.HttpExchange;
 import ru.yandex.practicum.manager.TaskManager;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public abstract class BaseHandler {
     protected TaskManager manager;
 
     public BaseHandler(TaskManager manager) {
         this.manager = manager;
+    }
+
+    protected void sendText(HttpExchange exchange, String body) throws IOException {
+        exchange.sendResponseHeaders(200, 0);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(body.getBytes());
+        }
+    }
+
+    protected void sendNotFound(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(404, -1);
     }
 }
