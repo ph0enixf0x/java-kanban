0050e3de2873ca298a52697bd572654636a10d7d
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-11 15:24:08 +0300

    Добавлены базовые методы отправки ответов при пересечении и отсутствии данных

--- src/ru/yandex/practicum/server/BaseHandler.java
+++ src/ru/yandex/practicum/server/BaseHandler.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.manager.TaskManager;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public abstract class BaseHandler {
     protected TaskManager manager;
@@ -14,13 +15,26 @@ public abstract class BaseHandler {
     }
 
     protected void sendText(HttpExchange exchange, String body) throws IOException {
+        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
         exchange.sendResponseHeaders(200, 0);
         try (OutputStream os = exchange.getResponseBody()) {
-            os.write(body.getBytes());
+            os.write(body.getBytes(StandardCharsets.UTF_8));
         }
+        exchange.close();
     }
 
     protected void sendNotFound(HttpExchange exchange) throws IOException {
         exchange.sendResponseHeaders(404, -1);
+        exchange.close();
+    }
+
+    protected void sendHasOverlaps(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(406, -1);
+        exchange.close();
+    }
+
+    protected void sendNoText(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(201, -1);
+        exchange.close();
     }
 }
