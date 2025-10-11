a1ba7336ed822e194a1417bdcc7a9915ce6517ea
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-11 14:21:03 +0300

    Добавлен обработчик эндпоинта "GET /tasks" и "GET /tasks/{id}"

--- src/ru/yandex/practicum/server/TasksHandler.java
+++ src/ru/yandex/practicum/server/TasksHandler.java
@@ -1,8 +1,11 @@
 package ru.yandex.practicum.server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import ru.yandex.practicum.manager.TaskManager;
+import ru.yandex.practicum.tasks.Task;
 
 import java.io.IOException;
 
@@ -13,6 +16,24 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Task.class, new TaskSerialiser())
+                .create();
+        switch (exchange.getRequestMethod()) {
+            case "GET":
+                String requestUri = exchange.getRequestURI().toString();
+                if (requestUri.contains("tasks/")) {
+                    Task task = manager.getTaskById(Integer.parseInt(
+                            requestUri.substring(requestUri.lastIndexOf("/") + 1)));
+                    if (task == null) {
+                        sendNotFound(exchange);
+                        return;
+                    }
+                    sendText(exchange, gson.toJson(task));
+                } else {
+                    sendText(exchange, gson.toJson(manager.getTasks()));
+                }
+                break;
+        }
     }
 }
