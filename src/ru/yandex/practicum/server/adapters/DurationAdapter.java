4e4140da40de85a7e636ff81286d17532ca85a95
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-13 14:43:48 +0300

    Добавлена проверка на null

--- src/ru/yandex/practicum/server/adapters/DurationAdapter.java
+++ src/ru/yandex/practicum/server/adapters/DurationAdapter.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.server.adapters;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -10,11 +11,19 @@ import java.time.Duration;
 public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
     public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
+        if (duration == null) {
+            jsonWriter.nullValue();
+            return;
+        }
         jsonWriter.value(duration.toMinutes());
     }
 
     @Override
     public Duration read(JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return null;
+        }
         return Duration.ofMinutes(jsonReader.nextInt());
     }
 }
--- src/ru/yandex/practicum/server/adapters/LocalDateTimeAdapter.java
+++ src/ru/yandex/practicum/server/adapters/LocalDateTimeAdapter.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.server.adapters;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -11,11 +12,19 @@ import java.time.temporal.ChronoUnit;
 public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
+        if (localDateTime == null) {
+            jsonWriter.nullValue();
+            return;
+        }
         jsonWriter.value(localDateTime.truncatedTo(ChronoUnit.SECONDS).toString());
     }
 
     @Override
     public LocalDateTime read(JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return null;
+        }
         return LocalDateTime.parse(jsonReader.nextString());
     }
 }
