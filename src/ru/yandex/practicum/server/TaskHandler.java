eff3d69af3f0db3e16ec863692ed436484c112b9
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-13 15:48:11 +0300

    Добавлены обработчики эндпоинта "/epics" и исправлены ошибки десериализации данных

--- src/ru/yandex/practicum/server/TaskHandler.java
+++ src/ru/yandex/practicum/server/TaskHandler.java
@@ -7,15 +7,18 @@ import com.sun.net.httpserver.HttpHandler;
 import ru.yandex.practicum.manager.TaskManager;
 import ru.yandex.practicum.server.adapters.DurationAdapter;
 import ru.yandex.practicum.server.adapters.LocalDateTimeAdapter;
+import ru.yandex.practicum.tasks.Epic;
 import ru.yandex.practicum.tasks.SubTask;
 import ru.yandex.practicum.tasks.Task;
+import ru.yandex.practicum.tasks.TaskStatus;
 
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
-public class TasksHandler extends BaseHandler implements HttpHandler {
-    public TasksHandler(TaskManager manager) {
+public class TaskHandler extends BaseHandler implements HttpHandler {
+    public TaskHandler(TaskManager manager) {
         super(manager);
     }
 
@@ -29,7 +32,7 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
         String method = exchange.getRequestMethod();
         String[] splitUri = requestUri.split("/");
         int taskId = 0;
-        boolean haveId = splitUri.length == 3;
+        boolean haveId = splitUri.length > 2;
         if (haveId) taskId = Integer.parseInt(splitUri[2]);
 
         switch (splitUri[1]) {
@@ -38,6 +41,7 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
                     case "GET":
                         if (haveId) {
                             Task task = manager.getTaskById(taskId);
+                            System.out.println(task);
                             if (task == null) {
                                 sendNotFound(exchange);
                                 return;
@@ -48,8 +52,17 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
                         }
                         break;
                     case "POST":
-                        Task task = gson.fromJson(new String(exchange.getRequestBody().readAllBytes()),
+                        Task decodedTask = gson.fromJson(new String(exchange.getRequestBody().readAllBytes()),
                                 Task.class);
+                        TaskStatus taskStatus = decodedTask.getStatus();
+
+                        Task task = new Task(decodedTask.getName(), decodedTask.getDescription(),
+                                decodedTask.getStartTime(), decodedTask.getDuration());
+                        task.setId(decodedTask.getId());
+                        if (taskStatus != null) {
+                            task.setStatus(taskStatus);
+                        }
+
                         if (task.getId() == 0) {
                             if (manager.createTask(task) == 0) {
                                 sendHasOverlaps(exchange);
@@ -72,7 +85,8 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
                     default:
                         sendMethodNotAllowed(exchange);
                 }
-            case "/subtasks":
+                break;
+            case "subtasks":
                 switch (method) {
                     case "GET":
                         if (haveId) {
@@ -87,8 +101,18 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
                         }
                         break;
                     case "POST":
-                        SubTask subtask = gson.fromJson(new String(exchange.getRequestBody().readAllBytes()),
+                        SubTask decodedSubtask = gson.fromJson(new String(exchange.getRequestBody().readAllBytes()),
                                 SubTask.class);
+                        TaskStatus subtaskStatus = decodedSubtask.getStatus();
+
+                        SubTask subtask = new SubTask(decodedSubtask.getName(), decodedSubtask.getDescription(),
+                                decodedSubtask.getStartTime(), decodedSubtask.getDuration(),
+                                decodedSubtask.getEpicId());
+                        subtask.setId(decodedSubtask.getId());
+                        if (subtaskStatus != null) {
+                            subtask.setStatus(subtaskStatus);
+                        }
+
                         if (subtask.getId() == 0) {
                             if (manager.createSubTask(subtask) == 0) {
                                 sendHasOverlaps(exchange);
@@ -99,6 +123,7 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
                         if (isUpdated(exchange, manager.updateSubTask(subtask))) {
                             sendCreated(exchange);
                         }
+                        break;
                     case "DELETE":
                         if (manager.deleteSubTaskById(taskId) == -1) {
                             sendNotFound(exchange);
@@ -108,7 +133,60 @@ public class TasksHandler extends BaseHandler implements HttpHandler {
                         break;
                     default:
                         sendMethodNotAllowed(exchange);
-            }
+                }
+                break;
+            case "epics":
+                switch (method) {
+                    case "GET":
+                        if (haveId) {
+                            Epic epic = manager.getEpicById(taskId);
+                            if (epic == null) {
+                                sendNotFound(exchange);
+                                return;
+                            }
+                            if (splitUri.length == 4) {
+                                sendText(exchange, gson.toJson(manager.getEpicSubTasks(taskId)));
+                                return;
+                            }
+                            sendText(exchange, gson.toJson(epic));
+                        } else {
+                            sendText(exchange, gson.toJson(manager.getEpics()));
+                        }
+                        break;
+                    case "POST":
+                        Epic decodedEpic = gson.fromJson(new String(exchange.getRequestBody().readAllBytes()),
+                                Epic.class);
+                        TaskStatus epicStatus = decodedEpic.getStatus();
+                        List<Integer> subtaskIds = decodedEpic.getSubtasksIds();
+
+                        Epic epic = new Epic(decodedEpic.getName(), decodedEpic.getDescription());
+                        epic.setId(decodedEpic.getId());
+                        if (epicStatus != null) {
+                            epic.setStatus(epicStatus);
+                        }
+                        if (subtaskIds != null) {
+                            subtaskIds.forEach(epic::addSubTask);
+                        }
+
+                        if (epic.getId() == 0) {
+                            manager.createEpic(epic);
+                            sendCreated(exchange);
+                            return;
+                        }
+                        if (isUpdated(exchange, manager.updateEpic(epic))) {
+                            sendCreated(exchange);
+                        }
+                        break;
+                    case "DELETE":
+                        if (manager.deleteEpicById(taskId) == -1) {
+                            sendNotFound(exchange);
+                            return;
+                        }
+                        sendOk(exchange);
+                        break;
+                    default:
+                        sendMethodNotAllowed(exchange);
+                }
         }
 
 
