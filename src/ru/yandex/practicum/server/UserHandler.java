4677e23b8dc4517f7e244a1e6f262916a888e73e
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-13 16:14:08 +0300

    Добавлен класс с обработчиками пользовательских эндпоинтов "GET /history" и "GET /prioritized"

---
+++ src/ru/yandex/practicum/server/UserHandler.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import ru.yandex.practicum.manager.TaskManager;
+
+import java.io.IOException;
+
+public class UserHandler extends BaseHandler implements HttpHandler {
+    public UserHandler(TaskManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestUri = exchange.getRequestURI().toString();
+        String method = exchange.getRequestMethod();
+        String[] splitUri = requestUri.split("/");
+
+        switch (splitUri[1]) {
+            case "history":
+                if (method.equals("GET")) {
+                    sendText(exchange, gson.toJson(manager.getHistory()));
+                } else {
+                    sendMethodNotAllowed(exchange);
+                }
+                break;
+            case "prioritized":
+                if (method.equals("GET")) {
+                    sendText(exchange, gson.toJson(manager.getPrioritizedTasks()));
+                } else {
+                    sendMethodNotAllowed(exchange);
+                }
+        }
+    }
+}
