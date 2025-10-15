a40960cd722045dc7936fa04a73afd146c533e3d
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-15 16:02:47 +0300

    Добавлено исключение пересечения создаваемой или изменяемой задачи по времени с уже существующими задачами

---
+++ src/ru/yandex/practicum/exception/HaveOverlapsException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.exception;
+
+public class HaveOverlapsException extends RuntimeException {
+    public HaveOverlapsException(String message) {
+        super(message);
+    }
+}
