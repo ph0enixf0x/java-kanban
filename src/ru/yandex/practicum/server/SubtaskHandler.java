e008075abe90d164526e15d6f0ee3bdb371b8134
Petr Yarkov <ph0enixf0x@yandex.ru>
2025-10-15 17:34:29 +0300

    Выведены в отдельный класс обработчики эндпоинта "/subtasks"

---
+++ src/ru/yandex/practicum/server/SubtaskHandler.java
@@ -0,0 +1,78 @@
+package ru.yandex.practicum.server;
+
+import com.sun.net.httpserver.HttpExchange;
+import ru.yandex.practicum.exception.HaveOverlapsException;
+import ru.yandex.practicum.exception.NotFoundException;
+import ru.yandex.practicum.manager.TaskManager;
+import ru.yandex.practicum.tasks.SubTask;
+import ru.yandex.practicum.tasks.TaskStatus;
+
+import java.io.IOException;
+
+public class SubtaskHandler extends  BaseHandler {
+    public SubtaskHandler(TaskManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestUri = exchange.getRequestURI().toString();
+        String method = exchange.getRequestMethod();
+        String[] splitUri = requestUri.split("/");
+        int taskId = 0;
+        if (splitUri.length > 2) taskId = Integer.parseInt(splitUri[2]);
+
+        switch (method) {
+            case "GET" -> subtasksGet(exchange, taskId);
+            case "POST" -> subtasksPost(exchange);
+            case "DELETE" -> subtasksDelete(exchange, taskId);
+            default -> sendMethodNotAllowed(exchange);
+        }
+    }
+
+    void subtasksGet(HttpExchange exchange, int taskId) throws IOException {
+        if (taskId == 0) {
+            sendText(exchange, gson.toJson(manager.getSubTasks()));
+            return;
+        }
+        try {
+            sendText(exchange, gson.toJson(manager.getSubTaskById(taskId)));
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+            sendNotFound(exchange);
+        }
+    }
+
+    void subtasksPost(HttpExchange exchange) throws IOException {
+        SubTask subtask = gson.fromJson(new String(exchange.getRequestBody().readAllBytes()),
+                SubTask.class);
+        if (subtask.getStatus() == null) subtask.setStatus(TaskStatus.NEW);
+        System.out.println(subtask.getStartTime());
+        System.out.println(subtask.getDuration());
+
+        try {
+            if (subtask.getId() == 0) {
+                manager.createSubTask(subtask);
+                sendCreated(exchange);
+                return;
+            }
+            manager.updateSubTask(subtask);
+        } catch (HaveOverlapsException e) {
+            System.out.println(e.getMessage());
+            sendHasOverlaps(exchange);
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+            sendNotFound(exchange);
+        }
+    }
+
+    void subtasksDelete(HttpExchange exchange, int taskId) throws IOException {
+        try {
+            manager.deleteSubTaskById(taskId);
+            sendOk(exchange);
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+            sendOk(exchange);
+        }
+    }
+}
